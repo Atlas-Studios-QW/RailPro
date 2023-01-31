546539af15faa6e33c6e25976f647eaf46471d5c
QuillanW <56843492+QuillanW@users.noreply.github.com>
2023-01-31 07:17:37 +0100

    Update

--- Assets/Scripts/System/NewTrainController.cs
+++ Assets/Scripts/System/NewTrainController.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Burst.CompilerServices;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class NewTrainController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float Speed = 10f;
 
-    // Update is called once per frame
-    void Update()
+    [Header("Turn Detector Origins")]
+    public GameObject ForwardDetector;
+    public GameObject BackwardDetector;
+
+    [HideInInspector]
+    public List<GameObject> ConnectedStock = new List<GameObject>();
+
+    private void Update()
     {
-        
+        //Check the direction, if it moves in reverse, check the animator at the rear
+        Transform DetectorOrigin = ForwardDetector.transform;
+
+        if (Speed < 0) { DetectorOrigin = BackwardDetector.transform; }
+
+        //Send a raycast to check for turns. If found, go through the turn
+        RaycastHit hit;
+        if (Physics.Raycast(DetectorOrigin.position, Vector3.down, out hit, 5f, LayerMask.GetMask("PathDetector")))
+        {
+            AnimationClip TurnAnimation = hit.transform.GetComponent<Animation>().GetClip("Turn");
+
+            GetComponent<Animation>().AddClip(TurnAnimation, "Turning");
+            GetComponent<Animation>().Play();
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, transform.position + transform.forward, Speed * Time.deltaTime);
+
     }
 }
